5d3284b067a3637269b77027db9e88cd5acfcffc
Brandon Courtney <brandon.h.courtney@gmail.com>
2014-04-07 16:54:15 -0700

    commit
    commit

---
+++ Ch05/5.6.2.rb
@@ -0,0 +1,6 @@
+# Excercises 5.6.2
+
+puts "So, what's you favorite number?"
+fav_num = gets.chomp.to_i
+bet_num = fav_num + 1
+puts "Well, " + fav_num.to_s + " is great, but don't you think " + bet_num.to_s + " could be a little bit better?"
